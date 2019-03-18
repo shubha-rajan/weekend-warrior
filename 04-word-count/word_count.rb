fbfd9f546a0fe21b9ecdf6bd3413e55ee724fa4f
Shubha Rajan <shubharajan94@gmail.com>
2019-03-17 18:33:26 -0700

    word count method passes all tests

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,12 @@
-
+def words(phrase)
+  counts = {}
+  words = phrase.split
+  words.each do |word|
+    if counts.keys.include?(word)
+      counts[word] += 1
+    else
+      counts[word] = 1
+    end
+  end
+  return counts
+end
