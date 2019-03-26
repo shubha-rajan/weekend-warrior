f8a60a7f74806370bd7bd0fa0455875a1495df36
Shubha Rajan <shubharajan94@gmail.com>
2019-03-26 14:53:00 -0700

    Passes all tests, puts newlines where needed

--- 05-alouette/alouette.rb
+++ 05-alouette/alouette.rb
@@ -1,10 +1,48 @@
 class Alouette
+  TOTAL_VERSES = 8
   def self.lines_for_verse(verse_num)
+    lines = [
+      "Et le dos!",
+      "Et la queue!",
+      "Et les pattes!",
+      "Et les ailes!",
+      "Et le cou!",
+      "Et les yeux!",
+      "Et le bec!",
+      "Et la tête!",
+    ]
+    return lines[TOTAL_VERSES - (verse_num + 1)...TOTAL_VERSES]
   end
 
   def self.verse(verse_num)
+    lines_for_verse = self.lines_for_verse(verse_num)
+    first_part = lines_for_verse[0].clone
+    first_part.slice!("Et ")
+    first_part.sub!("!", ".")
+    lines = [
+      "Je te plumerai #{first_part}\n",
+      "Je te plumerai #{first_part}\n",
+    ]
+    lines_for_verse.each do |line|
+      2.times do
+        lines << "#{line}\n"
+      end
+    end
+    lines = lines + ["Alouette!\n", "Alouette!\n", "A-a-a-ah"]
+    lines = lines.join
+    return lines
   end
 
   def self.sing
+    song = ""
+    refrain = "Alouette, gentille alouette,\nAlouette, je te plumerai."
+    song += "#{refrain}\n"
+    TOTAL_VERSES.times do |verse_num|
+      song += "\n#{refrain}\n" if verse_num != 0
+      song += "\n#{self.verse(verse_num)}\n"
+    end
+    song += "\n#{refrain}"
+
+    return song
   end
 end
--- 05-alouette/alouette_spec.rb
+++ 05-alouette/alouette_spec.rb
@@ -1,7 +1,7 @@
-gem 'minitest', '>= 5.0.0'
-require 'minitest/reporters'
-require 'minitest/autorun'
-require_relative 'alouette'
+gem "minitest", ">= 5.0.0"
+require "minitest/reporters"
+require "minitest/autorun"
+require_relative "alouette"
 
 Minitest::Reporters.use!
 
@@ -18,16 +18,14 @@ describe Alouette do
     end
 
     it "generates the correct line for the first verse" do
-      skip
-      Alouette.lines_for_verse(0).must_equal ['Et la tête!']
+      Alouette.lines_for_verse(0).must_equal ["Et la tête!"]
     end
 
     it "generates the correct lines for the third verse" do
-      skip
       expected_lines = [
         "Et les yeux!",
         "Et le bec!",
-        "Et la tête!"
+        "Et la tête!",
       ]
       Alouette.lines_for_verse(2).must_equal expected_lines
     end
@@ -35,24 +33,21 @@ describe Alouette do
 
   describe "verse" do
     it "returns a string" do
-      skip
       Alouette.verse(3).must_be_kind_of String
     end
 
     it "first two lines begin with 'Je te plumerai'" do
-      skip
       lines = Alouette.verse(3).split("\n")
 
       # If there aren't at least 2 lines, don't continue
       lines.length.must_be :>, 1, "Not enough lines for this test"
 
       2.times do |i|
-        lines[i].start_with?('Je te plumerai').must_equal true, "Line #{i} didn't start with 'Je te plumerai'"
+        lines[i].start_with?("Je te plumerai").must_equal true, "Line #{i} didn't start with 'Je te plumerai'"
       end
     end
 
     it "last three lines are 'Alouette! Alouette! A-a-a-ah'" do
-      skip
       lines = Alouette.verse(3).split("\n")
 
       # If there aren't at least 3 lines, don't continue
@@ -64,14 +59,13 @@ describe Alouette do
     end
 
     it "middle lines begin with 'Et ' and end with '!'" do
-      skip
       lines = Alouette.verse(3).split("\n")
 
       # If there aren't at least 6 lines, don't continue
       lines.length.must_be :>, 5, "Not enough lines for this test"
 
       # Slice off the first 2 and last 3 lines
-      lines = lines[2, lines.length-5]
+      lines = lines[2, lines.length - 5]
 
       lines.each do |line|
         line.start_with?("Et ").must_equal true, "Inner line didn't start with 'Et '"
@@ -80,7 +74,6 @@ describe Alouette do
     end
 
     it "generates the third verse" do
-      skip
       expected_verse = <<-__END_VERSE__
       Je te plumerai les yeux.
 Je te plumerai les yeux.
@@ -102,16 +95,14 @@ A-a-a-ah
   describe "sing" do
     # Load the canonical lyrics from disk
     let(:expected_lyrics) do
-      File.read(File.dirname(__FILE__) + '/alouette_lyrics.txt').strip
+      File.read(File.dirname(__FILE__) + "/alouette_lyrics.txt").strip
     end
 
     it "returns a string" do
-      skip
       Alouette.sing.must_be_kind_of String
     end
 
     it "begins and ends with the refrain" do
-      skip
       song = Alouette.sing
       refrain = "Alouette, gentille alouette,\nAlouette, je te plumerai."
       song.start_with?(refrain + "\n\n").must_equal true, "Song didn't begin with the refrain"
@@ -119,7 +110,6 @@ A-a-a-ah
     end
 
     it "generates the full lyrics" do
-      skip
       Alouette.sing.must_equal expected_lyrics
     end
   end
